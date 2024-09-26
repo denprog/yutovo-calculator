83ef1dde46280ea309736a1fe18ef986528bc169
Denis Gordenin <dgordenin@gmail.com>
2024-09-26 20:32:03 +0300

    Add the gravitational acceleration constant.

--- test/real.cpp
+++ test/real.cpp
@@ -387,6 +387,13 @@ TEST_F(CalcTestReal, variables18)
     ASSERT_TRUE(r.ToStdString(3, 3) == "6.674E-11((м^3)/(кг*сек^2))") << r.ToStdString(3, 3);
 }
 
+TEST_F(CalcTestReal, variables19)
+{
+    parser.SetLocale(Language::Russian);
+    auto r = parser.Parse(ElementId{0, 0, 1}, U"g_a;");
+    ASSERT_TRUE(r.ToStdString(3, 3) == "9.807E+0((м)/(сек^2))") << r.ToStdString(3, 3);
+}
+
 TEST_F(CalcTestReal, symbols1)
 {
     EXPECT_THROW(parser.Parse(ElementId{0, 0, 1}, U"•;"), yutovo_calculator::SyntaxException) << parser.Parse(ElementId{0, 0, 1}, U"•;").ToStdString(3, 3);
