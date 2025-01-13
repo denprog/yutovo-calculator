c327a6869cb099d8955527e3c1563cd4e0e4f16d
Denis Gordenin <dgordenin@gmail.com>
2025-01-13 23:18:43 +0300

    Format.

--- src/annotation.h
+++ src/annotation.h
@@ -5,306 +5,308 @@
 
 namespace yutovo_calculator
 {
-	//Annotation for nodes. Used in purposes of searching a node by a text position.
-	template<typename Number>
-	struct Annotation
+
+//Annotation for nodes. Used in purposes of searching a node by a text position.
+template<typename Number>
+struct Annotation
+{
+	typedef boost::variant<
+		boost::recursive_wrapper<NumberNode<Number>>, 
+		boost::recursive_wrapper<IdentifierNode<Number>>, 
+		boost::recursive_wrapper<UnaryOperationNode<Number>>, 
+		boost::recursive_wrapper<OperationNode<Number>>, 
+		boost::recursive_wrapper<PostfixOperationNode<Number>>, 
+		boost::recursive_wrapper<MixedDivivsionNode<Number>>, 
+		boost::recursive_wrapper<ImplicitStringMulNode<Number>>, 
+		boost::recursive_wrapper<ImplicitDivMulNode<Number>>, 
+		boost::recursive_wrapper<ImplicitMulNode<Number>>, 
+		boost::recursive_wrapper<ImplicitFractionMulNode<Number>>, 
+		boost::recursive_wrapper<ImplicitFunctionMulNode<Number>>, 
+		boost::recursive_wrapper<ImplicitPostFunctionMulNode<Number>>, 
+		boost::recursive_wrapper<FunctionCallNode<Number>>, 
+		boost::recursive_wrapper<FunctionCallStringNode<Number>>, 
+		boost::recursive_wrapper<NoFencesFunctionCallNode<Number>>, 
+		boost::recursive_wrapper<CompareNode<Number>>, 
+		boost::recursive_wrapper<LoopNode<Number>>, 
+		boost::recursive_wrapper<ExpressionNode<Number>>>
+		Operand;
+
+	Annotation(std::u32string::iterator _first, std::u32string::iterator _last, LogicalId& _id, ParserContext** _parser_context) : 
+		id(_id),
+		first(_first),
+		last(_last),
+		parser_context(_parser_context)
 	{
-		typedef boost::variant<
-			boost::recursive_wrapper<NumberNode<Number>>, 
-			boost::recursive_wrapper<IdentifierNode<Number>>, 
-			boost::recursive_wrapper<UnaryOperationNode<Number>>, 
-			boost::recursive_wrapper<OperationNode<Number>>, 
-			boost::recursive_wrapper<PostfixOperationNode<Number>>, 
-			boost::recursive_wrapper<MixedDivivsionNode<Number>>, 
-			boost::recursive_wrapper<ImplicitStringMulNode<Number>>, 
-			boost::recursive_wrapper<ImplicitDivMulNode<Number>>, 
-			boost::recursive_wrapper<ImplicitMulNode<Number>>, 
-			boost::recursive_wrapper<ImplicitFractionMulNode<Number>>, 
-			boost::recursive_wrapper<ImplicitFunctionMulNode<Number>>, 
-			boost::recursive_wrapper<ImplicitPostFunctionMulNode<Number>>, 
-			boost::recursive_wrapper<FunctionCallNode<Number>>, 
-			boost::recursive_wrapper<FunctionCallStringNode<Number>>, 
-			boost::recursive_wrapper<NoFencesFunctionCallNode<Number>>, 
-			boost::recursive_wrapper<CompareNode<Number>>, 
-			boost::recursive_wrapper<LoopNode<Number>>, 
-			boost::recursive_wrapper<ExpressionNode<Number>>>
-			Operand;
+	}
 
-		Annotation(std::u32string::iterator _first, std::u32string::iterator _last, LogicalId& _id, ParserContext** _parser_context) : 
-			id(_id),
-			first(_first),
-			last(_last),
-			parser_context(_parser_context)
-		{
-		}
+	//Updates the position in the expression.
+	void UpdatePosition(std::u32string::iterator p, ExpressionPosition const& position) const
+	{
+		UpdatePosition(p, (ExpressionPosition&)position);
+	}
+	
+	//Updates the position in the expression.
+	void UpdatePosition(std::u32string::iterator p, ExpressionPosition& position) const
+	{
+		CheckBreak(*parser_context);
 
-		//Updates the position in the expression.
-		void UpdatePosition(std::u32string::iterator p, ExpressionPosition const& position) const
-		{
-			UpdatePosition(p, (ExpressionPosition&)position);
-		}
+		int i = 0, j = 0;
+		std::u32string::iterator k = first;
 		
-		//Updates the position in the expression.
-		void UpdatePosition(std::u32string::iterator p, ExpressionPosition& position) const
+		while (k != p)
 		{
-			CheckBreak(*parser_context);
-
-			int i = 0, j = 0;
-			std::u32string::iterator k = first;
-			
-			while (k != p)
+			char ch = *k;
+			if (ch == '\r')
 			{
-				char ch = *k;
-				if (ch == '\r')
-				{
-					++k;
-					if (ch == '\n')
-						++k;
-					++j;
-				}
-			
 				++k;
-				++i;
+				if (ch == '\n')
+					++k;
+				++j;
 			}
-			
-			position.id = id;
-			position.pos = i;
-			position.line = j;
+		
+			++k;
+			++i;
 		}
 		
-		void operator()(Operand& op, std::u32string::iterator pos) const
+		position.id = id;
+		position.pos = i;
+		position.line = j;
+	}
+	
+	void operator()(Operand& op, std::u32string::iterator pos) const
+	{
+		boost::apply_visitor(OperandVisitor<Number>(this, pos), op);
+	}
+
+	void operator()(IdentifierNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(NumberNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ExpressionNode<Number>& expr, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, expr);
+	}
+
+	void operator()(UnaryOperationNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(PostfixOperationNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(OperationNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(MixedDivivsionNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitStringMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitDivMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitFractionMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitFunctionMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(ImplicitPostFunctionMulNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(FunctionCallNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(FunctionCallStringNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(NoFencesFunctionCallNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(CompareNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(LoopNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(VariableNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	void operator()(DefinitionNode<Number>& op, std::u32string::iterator pos) const
+	{
+		UpdatePosition(pos, op);
+	}
+
+	template<typename Num>
+	struct OperandVisitor
+	{
+		OperandVisitor(Annotation<Num> const* _annotation, std::u32string::iterator _iter) : annotation(_annotation), iter(_iter)
 		{
-			boost::apply_visitor(OperandVisitor<Number>(this, pos), op);
+		}
+		
+		void operator()(OperationNode<Num> const& op) const
+		{
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(IdentifierNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(NumberNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(NumberNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(UnaryOperationNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ExpressionNode<Number>& expr, std::u32string::iterator pos) const
+		void operator()(PostfixOperationNode<Num> const& op) const
 		{
-			UpdatePosition(pos, expr);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(UnaryOperationNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(MixedDivivsionNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(PostfixOperationNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitStringMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(OperationNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitDivMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(MixedDivivsionNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitStringMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitFractionMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitDivMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitFunctionMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ImplicitPostFunctionMulNode<Number>& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitFractionMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(IdentifierNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitFunctionMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(FunctionCallNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(ImplicitPostFunctionMulNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(FunctionCallStringNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(FunctionCallNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(NoFencesFunctionCallNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(FunctionCallStringNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(CompareNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(NoFencesFunctionCallNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(LoopNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(CompareNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(ExpressionNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(LoopNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(DefinitionNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(VariableNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(VariableNode<Num> const& op) const
 		{
-			UpdatePosition(pos, op);
+			annotation->UpdatePosition(iter, op);
 		}
 
-		void operator()(DefinitionNode<Number>& op, std::u32string::iterator pos) const
+		void operator()(Num const& op) const
 		{
-			UpdatePosition(pos, op);
+			//((Number)op).SetPrecision(10);
 		}
-
-		template<typename Num>
-		struct OperandVisitor
-		{
-			OperandVisitor(Annotation<Num> const* _annotation, std::u32string::iterator _iter) : annotation(_annotation), iter(_iter)
-			{
-			}
-			
-			void operator()(OperationNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(NumberNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(UnaryOperationNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(PostfixOperationNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(MixedDivivsionNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitStringMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitDivMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitFractionMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitFunctionMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ImplicitPostFunctionMulNode<Number>& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(IdentifierNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(FunctionCallNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(FunctionCallStringNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(NoFencesFunctionCallNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(CompareNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(LoopNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(ExpressionNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(DefinitionNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(VariableNode<Num> const& op) const
-			{
-				annotation->UpdatePosition(iter, op);
-			}
-
-			void operator()(Num const& op) const
-			{
-				//((Number)op).SetPrecision(10);
-			}
-			
-			typedef void result_type;
-			Annotation<Num> const* annotation;
-			std::u32string::iterator iter;
-		};
-
-		template<typename, typename>
-		struct result
-		{
-			typedef void type;
-		};
-
-		LogicalId id;
-		std::u32string::iterator first;
-		std::u32string::iterator last;
-		ParserContext** parser_context;
+		
+		typedef void result_type;
+		Annotation<Num> const* annotation;
+		std::u32string::iterator iter;
 	};
+
+	template<typename, typename>
+	struct result
+	{
+		typedef void type;
+	};
+
+	LogicalId id;
+	std::u32string::iterator first;
+	std::u32string::iterator last;
+	ParserContext** parser_context;
+};
+
 }
 
 #endif
--- src/parser_exception.h
+++ src/parser_exception.h
@@ -7,136 +7,138 @@
 
 namespace yutovo_calculator
 {
-	typedef unsigned int uint;
 
-	enum ParserExceptionCode
-	{
-		None = -1, 
-		SyntaxError = 1, 
-		WrongArgumentsCount = 2, 
-		UnknownIdentifier = 3, 
-		ExpressionExpected = 4,
-		DivisionByZero = 100, 
-		Overflow = 101, 
-		ArgumentIsOver = 102, 
-		ConversionDoesNotFit = 103,
-		UnitsAreIncompatible = 201,
-		CannotCastToUnit = 202,
-		UnknownLanguage = 301,
-		TimeExceed = 401,
-		Break = 501
-	};
+typedef unsigned int uint;
+
+enum ParserExceptionCode
+{
+	None = -1, 
+	SyntaxError = 1, 
+	WrongArgumentsCount = 2, 
+	UnknownIdentifier = 3, 
+	ExpressionExpected = 4,
+	DivisionByZero = 100, 
+	Overflow = 101, 
+	ArgumentIsOver = 102, 
+	ConversionDoesNotFit = 103,
+	UnitsAreIncompatible = 201,
+	CannotCastToUnit = 202,
+	UnknownLanguage = 301,
+	TimeExceed = 401,
+	Break = 501
+};
+
+struct ParserException
+{
+	ParserException() = default;
 	
-	struct ParserException
+	ParserException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) :
+		id(_id),
+		ex_id(_ex_id),
+		pos(_pos),
+		line(_line)
 	{
-		ParserException() = default;
-		
-		ParserException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) :
-			id(_id),
-			ex_id(_ex_id),
-			pos(_pos),
-			line(_line)
-		{
-		}
+	}
 
-		ParserException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _size, int _line) :
-			id(_id),
-			ex_id(_ex_id),
-			pos(_pos),
-			size(_size),
-			line(_line)
-		{
-		}
+	ParserException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _size, int _line) :
+		id(_id),
+		ex_id(_ex_id),
+		pos(_pos),
+		size(_size),
+		line(_line)
+	{
+	}
 
-		ParserException(LogicalId _id, ParserExceptionCode _ex_id) :
-			id(_id),
-			ex_id(_ex_id)
-		{
-		}
+	ParserException(LogicalId _id, ParserExceptionCode _ex_id) :
+		id(_id),
+		ex_id(_ex_id)
+	{
+	}
 
-		ParserException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _line) :
-			id(_id),
-			ex_id(_ex_id),
-			description(_description),
-			pos(_pos),
-			line(_line)
-		{
-		}
+	ParserException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _line) :
+		id(_id),
+		ex_id(_ex_id),
+		description(_description),
+		pos(_pos),
+		line(_line)
+	{
+	}
 
-		ParserException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _size, int _line) :
-			id(_id),
-			ex_id(_ex_id),
-			description(_description),
-			pos(_pos),
-			size(_size),
-			line(_line)
-		{
-		}
+	ParserException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _size, int _line) :
+		id(_id),
+		ex_id(_ex_id),
+		description(_description),
+		pos(_pos),
+		size(_size),
+		line(_line)
+	{
+	}
 
-		friend bool operator==(const ParserException& e1, const ParserException& e2)
-		{
-			return e1.id == e2.id && e1.ex_id == e2.ex_id && e1.pos == e2.pos && e1.line == e2.line;
-		}
-		
-		LogicalId id;
-		ParserExceptionCode ex_id = None;
-		int pos = -1;
-		int size = 0; //0 means whole substring
-		int line = -1;
-		std::u32string description;
-	};
+	friend bool operator==(const ParserException& e1, const ParserException& e2)
+	{
+		return e1.id == e2.id && e1.ex_id == e2.ex_id && e1.pos == e2.pos && e1.line == e2.line;
+	}
 	
-	struct SyntaxException : public ParserException
+	LogicalId id;
+	ParserExceptionCode ex_id = None;
+	int pos = -1;
+	int size = 0; //0 means whole substring
+	int line = -1;
+	std::u32string description;
+};
+
+struct SyntaxException : public ParserException
+{
+	SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) : 
+		ParserException(_id, _ex_id, _pos, _line)
 	{
-		SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) : 
-			ParserException(_id, _ex_id, _pos, _line)
-		{
-		}
+	}
 
-		SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _size, int _line) : 
-			ParserException(_id, _ex_id, _pos, _size, _line)
-		{
-		}
-
-		SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _line) : 
-			ParserException(_id, _ex_id, _description, _pos, _line)
-		{
-		}
-
-		SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _size, int _line) : 
-			ParserException(_id, _ex_id, _description, _pos, _size, _line)
-		{
-		}
-	};
-	
-	struct MathException : public ParserException
+	SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _size, int _line) : 
+		ParserException(_id, _ex_id, _pos, _size, _line)
 	{
-		MathException(ParserExceptionCode _id) : ParserException(LogicalId{}, _id, -1, -1)
-		{
-		}
+	}
 
-		MathException(LogicalId _id, ParserExceptionCode _ex_id) : ParserException(_id, _ex_id)
-		{
-		}
-
-		MathException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) : ParserException(_id, _ex_id, _pos, _line)
-		{
-		}
-	};
-
-	struct TimeExceedException : public ParserException
+	SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _line) : 
+		ParserException(_id, _ex_id, _description, _pos, _line)
 	{
-		TimeExceedException() : ParserException(LogicalId{}, ParserExceptionCode::TimeExceed)
-		{
-		}
-	};
+	}
 
-	struct BreakException : public ParserException
+	SyntaxException(LogicalId _id, ParserExceptionCode _ex_id, std::u32string _description, int _pos, int _size, int _line) : 
+		ParserException(_id, _ex_id, _description, _pos, _size, _line)
 	{
-		BreakException() : ParserException(LogicalId{}, ParserExceptionCode::Break)
-		{
-		}
-	};
+	}
+};
+
+struct MathException : public ParserException
+{
+	MathException(ParserExceptionCode _id) : ParserException(LogicalId{}, _id, -1, -1)
+	{
+	}
+
+	MathException(LogicalId _id, ParserExceptionCode _ex_id) : ParserException(_id, _ex_id)
+	{
+	}
+
+	MathException(LogicalId _id, ParserExceptionCode _ex_id, int _pos, int _line) : ParserException(_id, _ex_id, _pos, _line)
+	{
+	}
+};
+
+struct TimeExceedException : public ParserException
+{
+	TimeExceedException() : ParserException(LogicalId{}, ParserExceptionCode::TimeExceed)
+	{
+	}
+};
+
+struct BreakException : public ParserException
+{
+	BreakException() : ParserException(LogicalId{}, ParserExceptionCode::Break)
+	{
+	}
+};
+
 }
 
 #endif
