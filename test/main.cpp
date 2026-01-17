629cce6c95b0b818ee2758bcd306f592e138e8ad
dgordenin <dprmailbox@gmail.com>
2026-01-17 20:24:37 +0300

    Fix building in Windows.

--- test/main.cpp
+++ test/main.cpp
@@ -7,22 +7,6 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#ifdef _WIN32
-#include <boost/throw_exception.hpp>
-
-namespace boost
-{
-    BOOST_NORETURN void throw_exception(std::exception const& e)
-    {
-        throw e;
-    }
-
-    BOOST_NORETURN void throw_exception(std::exception const& e, boost::source_location const&)
-    {
-        throw e;
-    }
-}
-#endif
 
 int main(int argc, char** argv)
 {
